3019ae06dcd0826e036feee1bd7d714adf7faea6
Iacopo <iacopo.ghila@gmail.com>
2020-12-12 13:08:07 +0100

    Milestone 2 done

--- js/script.js
+++ js/script.js
@@ -5,88 +5,108 @@
 // Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
 // Click sul contatto mostra la conversazione del contatto cliccato
 
-// const contacts: [
-// 	{
-// 		name: 'Michele',
-// 		avatar: '_1',
-// 		visible: true,
-// 		messages: [
-// 			{
-// 				date: '10/01/2020 15:30:55',
-// 				text: 'Hai portato a spasso il cane?',
-// 				status: 'sent'
-// 			},
-// 			{
-// 				date: '10/01/2020 15:50:00',
-// 				text: 'Ricordati di dargli da mangiare',
-// 				status: 'sent'
-// 			},
-// 			{
-// 				date: '10/01/2020 16:15:22',
-// 				text: 'Tutto fatto!',
-// 				status: 'received'
-// 			}
-// 		],
-// 	},
-// 	{
-// 		name: 'Fabio',
-// 		avatar: '_2',
-// 		visible: true,
-// 		messages: [
-// 			{
-// 				date: '20/03/2020 16:30:00',
-// 				text: 'Ciao come stai?',
-// 				status: 'sent'
-// 			},
-// 			{
-// 				date: '20/03/2020 16:30:55',
-// 				text: 'Bene grazie! Stasera ci vediamo?',
-// 				status: 'received'
-// 			},
-// 			{
-// 				date: '20/03/2020 16:35:00',
-// 				text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-// 				status: 'sent'
-// 			}
-// 		],
-// 	},
-// 	{
-// 		name: 'Samuele',
-// 		avatar: '_3',
-// 		visible: true,
-// 		messages: [
-// 			{
-// 				date: '28/03/2020 10:10:40',
-// 				text: 'La Marianna va in campagna',
-// 				status: 'received'
-// 			},
-// 			{
-// 				date: '28/03/2020 10:20:10',
-// 				text: 'Sicuro di non aver sbagliato chat?',
-// 				status: 'sent'
-// 			},
-// 			{
-// 				date: '28/03/2020 16:15:22',
-// 				text: 'Ah scusa!',
-// 				status: 'received'
-// 			}
-// 		],
-// 	},
-// 	{
-// 		name: 'Luisa',
-// 		avatar: '_4',
-// 		visible: true,
-// 		messages: [
-// 			{
-// 				date: '10/01/2020 15:30:55',
-// 				text: 'Lo sai che ha aperto una nuova pizzeria?',
-// 				status: 'sent'
-// 			},
-// 			{
-// 				date: '10/01/2020 15:50:00',
-// 				text: 'Si, ma preferirei andare al cinema',
-// 				status: 'received'
-// 			}
-// 		],
-// 	},
-// ]
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const app = new Vue({
+
+        el: '#container',
+        data:{
+            contactIndex: 0,
+            contacts: [
+            	{
+            		name: 'Uomo pipistrello',
+            		avatar: 'img/batman-icon.png',
+            		visible: true,
+            		messages: [
+            			{
+            				date: '10/01/2020 15:30:55',
+            				text: 'Sei uscito oggi?',
+            				status: 'sent'
+            			},
+            			{
+            				date: '10/01/2020 15:50:00',
+            				text: 'Un po\' di luce ti farebbe bene',
+            				status: 'sent'
+            			},
+            			{
+            				date: '10/01/2020 16:15:22',
+            				text: 'Sono Batman!',
+            				status: 'received'
+                        },
+                        
+            		],
+            	},
+            	{
+            		name: 'Uomo con la tutina',
+            		avatar: 'img/spiderman-avatar.webp',
+            		visible: true,
+            		messages: [
+            			{
+            				date: '20/03/2020 16:30:00',
+            				text: 'Ciao come stai?',
+            				status: 'sent'
+            			},
+            			{
+            				date: '20/03/2020 16:30:55',
+            				text: 'Bene grazie! Stasera ci vediamo?',
+            				status: 'received'
+            			},
+            			{
+            				date: '20/03/2020 16:35:00',
+            				text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+            				status: 'sent'
+            			}
+            		],
+            	},
+            	{
+            		name: 'Armored boy',
+            		avatar: 'img/ironman.webp',
+            		visible: true,
+            		messages: [
+            			{
+            				date: '28/03/2020 10:10:40',
+            				text: 'La Marianna va in campagna',
+            				status: 'received'
+            			},
+            			{
+            				date: '28/03/2020 10:20:10',
+            				text: 'Sicuro di non aver sbagliato chat?',
+            				status: 'sent'
+            			},
+            			{
+            				date: '28/03/2020 16:15:22',
+            				text: 'Ah scusa!',
+            				status: 'received'
+            			}
+            		],
+            	},
+            	{
+            		name: 'Me stesso',
+            		avatar: 'img/deadpool.png',
+            		visible: true,
+            		messages: [
+            			{
+            				date: '10/01/2020 15:30:55',
+            				text: 'Sei il migliore di tutti',
+            				status: 'sent'
+            			},
+            			{
+            				date: '10/01/2020 15:50:00',
+            				text: 'Si, lo so',
+            				status: 'received'
+            			}
+            		],
+            	},
+            ],
+
+        }, 
+        methods: {
+            changeContact(index){
+                this.contactIndex = index;
+            }
+        }
+          
+    });
+
+});
