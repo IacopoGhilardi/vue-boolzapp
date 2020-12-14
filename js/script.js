bd0dceeb6b905dbd283e9024c79c8cb27f3fb0db
Iacopo <iacopo.ghila@gmail.com>
2020-12-14 14:34:09 +0100

    Fatta risposta e aggiunto dayjs

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@
 // Milestone 2
 // Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
 // Click sul contatto mostra la conversazione del contatto cliccato
+// Milestone 3
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -12,6 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         el: '#container',
         data:{
+			userInput: "",
             contactIndex: 0,
             contacts: [
             	{
@@ -101,11 +105,32 @@ document.addEventListener('DOMContentLoaded', () => {
             	},
             ],
 
-        }, 
+		},
         methods: {
             changeContact(index){
                 this.contactIndex = index;
-            }
+			},
+			reciveMessage() {
+				setTimeout(() => {
+					
+					this.contacts[this.contactIndex].messages.push(
+						{
+							data: dayjs().format("DD-MM-YYYY HH:mm:ss"),
+							text: 'Ok',
+							status: 'received'
+						});
+						console.log(this.contacts[this.contactIndex].messages);
+				}, 1000);
+			},
+			sendMessage() {
+				this.contacts[this.contactIndex].messages.push(
+					{
+						data: dayjs().format("DD-MM-YYYY HH:mm:ss"),
+						text: this.userInput,
+						status: 'sent'
+					});
+				this.userInput = '';
+			}
         }
           
     });
